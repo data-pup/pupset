f679f2192aeda31f0a6fb1b20c952078254edba3
data-pup <kevinmartinucr@gmail.com>
2018-03-30 16:09:46 -0700

    Added a simple pair of functions to print the arguments given to the program.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+
+fn collect_args() -> Vec<String> {
+    let args = env::args()
+        .skip(1)
+        .collect();
+    args
+}
+
+fn print_args(args: &Vec<String>) {
+    println!("Received Arguments:");
+    let mut i = 0;
+    for a in args.iter() {
+        println!("[{}] : {}", i, *a);
+        i += 1;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = collect_args();
+    print_args(&args);
 }
