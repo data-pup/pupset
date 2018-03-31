a0ecb1a7069544e3e3eeeeef2c691cfd764ed8f3
data-pup <kevinmartinucr@gmail.com>
2018-03-30 19:36:44 -0700

    Cleaned up the input reading code slightly.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::io;
 use std::io::BufRead;
 use std::env;
 
+/// Collect the arguments given to the program.
 fn collect_args() -> Vec<String> {
     let args = env::args()
         .skip(1)
@@ -9,6 +10,7 @@ fn collect_args() -> Vec<String> {
     args
 }
 
+/// Print the arguments given to the program.
 fn print_args(args: &Vec<String>) {
     println!("Received Arguments:");
     let mut i = 0;
@@ -18,26 +20,25 @@ fn print_args(args: &Vec<String>) {
     }
 }
 
+/// Read input from stdin and print it to stdout.
 fn print_stdin_contents() -> Result<(), io::Error> {
     let stdin = io::stdin();
     let handle = stdin.lock();
+    println!("Reading stdin:");
     for line_result in handle.lines() {
-        match line_result {
-            Ok(line) => println!("{}", line),
-            Err(err) => {
-                return Err(err);
-            },
-        }
+        let line = line_result?;
+        println!("{}", line);
     }
     Ok(())
 }
 
+/// Print an I/O error if a problem occurred while reading from stdin.
+fn handle_string_error(err: io::Error) {
+    eprintln!("Error reading input: {}", err);
+}
+
 fn main() {
-    let args = collect_args();
+    let args: Vec<String> = collect_args();
     print_args(&args);
-    println!("Reading stdin:");
-    print_stdin_contents().unwrap_or_else(|err| {
-        eprintln!("Error reading input: {}", err);
-        std::process::exit(1);
-    });
+    print_stdin_contents().unwrap_or_else(|err| handle_string_error(err));
 }
