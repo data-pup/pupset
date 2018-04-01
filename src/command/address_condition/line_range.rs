652b853f883a30203f258ed22942a7ad33d35bd6
data-pup <kevinmartinucr@gmail.com>
2018-03-31 22:09:13 -0700

    Added more logic for the min and max checking.

--- src/command/address_condition/line_range.rs
+++ src/command/address_condition/line_range.rs
@@ -11,12 +11,7 @@ pub struct LineRange {
 
 impl AddressCondition for LineRange {
     fn applies(&self, current_line: Address) -> bool {
-        match self.min.is_inclusive {
-            true if  (self.min.val <= current_line) != true => return false,
-            false if (self.min.val < current_line)  != true => return false,
-            _ => { }
-        }
-        true
+        self.check_min(current_line) && self.check_max(current_line)
     }
 }
 
@@ -25,6 +20,24 @@ impl TwoAddressCondition for LineRange {
         Self {
             min: RangeBounds { val: a, is_inclusive: true  },
             max: RangeBounds { val: b, is_inclusive: false },
+        }
+    }
+}
+
+impl LineRange {
+    fn check_min(&self, current_line: Address) -> bool {
+        match self.min.is_inclusive {
+            true if  (self.min.val <= current_line) != true => return false,
+            false if (self.min.val < current_line)  != true => return false,
+            _ => { true }
+        }
+    }
+
+    fn check_max(&self, current_line: Address) -> bool {
+        match self.max.is_inclusive {
+            true if  (self.max.val >= current_line) != true => return false,
+            false if (self.max.val > current_line)  != true => return false,
+            _ => { true }
         }
     }
 }
@@ -42,6 +55,6 @@ mod tests {
         assert_eq!(range.applies(4),  false);
         assert_eq!(range.applies(5),  true);
         assert_eq!(range.applies(9),  true);
-        // assert_eq!(range.applies(10), false);
+        assert_eq!(range.applies(10), false);
     }
 }
