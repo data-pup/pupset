ec0bf7744ca6677af7018f1045bcd73c6426dbde
data-pup <kevinmartinucr@gmail.com>
2018-04-01 13:33:32 -0700

    All tests are now passing. Address conditions are now optional, and generic to the condition trait that we have already implemented at this point.

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -13,9 +13,12 @@ pub struct Command {
 }
 
 impl Command {
-    pub fn new(line:Address) -> Self {
+    pub fn new(addr:Option<Address>) -> Self {
         Self {
-            cond: Some(Box::new(LineNumber::new(line))),
+            cond: match addr {
+                Some(a) => Some(Box::new(LineNumber::new(a))),
+                None    => None,
+            }
         }
     }
 
@@ -35,7 +38,7 @@ mod comm_address_condition_tests {
     fn line_number_condition_works() {
         let addrs: Vec<Address> = (0..10).collect();
         let comm_line: Address = 5;
-        let comm = Command::new(comm_line);
+        let comm = Command::new(Some(comm_line));
         let expected_results: Vec<bool> =
             addrs.iter()
             .map(|addr: &Address| *addr == comm_line)
@@ -50,7 +53,7 @@ mod comm_address_condition_tests {
     #[test]
     fn empty_condition_always_runs() {
         let addrs: Vec<Address> = (0..10).collect();
-        let comm = Command::new(0);
+        let comm = Command::new(None);
         for curr_addr in addrs.iter() {
             assert_eq!(comm.should_run(*curr_addr), true);
         }
