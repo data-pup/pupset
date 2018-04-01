6c319fdb622e72ad7cec1c6133384858ecf808a3
data-pup <kevinmartinucr@gmail.com>
2018-04-01 16:24:13 -0700

    Adding tests to ensure that closures are included in the address condition string.

--- src/command/address_condition/parse.rs
+++ src/command/address_condition/parse.rs
@@ -2,35 +2,49 @@ use command::address_condition::{
     Address
 };
 
+pub type ParseResult = Result<AddressList, ParseError>;
 pub type AddressList = Vec<Address>;
-
 #[derive(Debug, PartialEq)]
 pub enum ParseError {
     ArgEmpty,
+    ArgTooShort,
     InvalidLowerBounds,
     InvalidUpperBounds,
     InvalidAddressNumber,
 }
 
-pub type ParseResult = Result<AddressList, ParseError>;
-
 static LOWER_BOUNDS_CHARS: [char; 2] = ['[', '('];
 static UPPER_BOUNDS_CHARS: [char; 2] = [']', ')'];
+static MINIMUM_LENGTH: usize = 3;
 
 pub fn parse_arg(arg: String) -> ParseResult {
     if arg.is_empty() { return Err(ParseError::ArgEmpty); }
+    if arg.len() < MINIMUM_LENGTH { return Err(ParseError::ArgTooShort); }
     Ok(vec![])
 }
 
 #[cfg(test)]
 mod parse_tests {
     use command::address_condition::parse::*;
-    // use command::address_condition::Address;
 
     #[test]
     fn empty_string_returns_err() {
         let actual_result:   ParseResult = parse_arg(String::from(""));
         let expected_result: ParseResult = Err(ParseError::ArgEmpty);
+        assert_eq!(actual_result, expected_result);
+    }
+
+    #[test]
+    fn empty_closure_returns_err() {
+        let actual_result:   ParseResult = parse_arg(String::from("[]"));
+        let expected_result: ParseResult = Err(ParseError::ArgTooShort);
+        assert_eq!(actual_result, expected_result);
+    }
+
+    #[test]
+    fn missing_closure_returns_err() {
+        let actual_result:   ParseResult = parse_arg(String::from("1"));
+        let expected_result: ParseResult = Err(ParseError::ArgTooShort);
         assert_eq!(actual_result, expected_result);
     }
 
