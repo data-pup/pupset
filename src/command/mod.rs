3b2a5ba3fb00df947382f43f200dfcf87f9fab16
data-pup <kevinmartinucr@gmail.com>
2018-04-01 13:30:41 -0700

    Single line number test seems to work as expected. Now working on refactoring to add the possibility of no address condition being given to the command object.

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -1,6 +1,7 @@
 pub mod address_condition;
 use self::address_condition::{
-    // AddressCondition,
+    Address,
+    AddressCondition,
     OneAddressCondition,
     LineNumber,
     // LineRange,
@@ -8,15 +9,50 @@ use self::address_condition::{
 };
 
 pub struct Command {
-    cond: LineNumber,
+    cond: Option<Box<AddressCondition>>,
+}
+
+impl Command {
+    pub fn new(line:Address) -> Self {
+        Self {
+            cond: Some(Box::new(LineNumber::new(line))),
+        }
+    }
+
+    pub fn should_run(&self, curr_line:Address) -> bool {
+        match self.cond {
+            Some(ref cond) => cond.applies(curr_line),
+            None           => true,
+        }
+    }
 }
 
 #[cfg(test)]
-mod tests {
+mod comm_address_condition_tests {
     use command::*;
 
     #[test]
-    fn it_works() {
-        Command { cond: LineNumber::new(0_32) };
+    fn line_number_condition_works() {
+        let addrs: Vec<Address> = (0..10).collect();
+        let comm_line: Address = 5;
+        let comm = Command::new(comm_line);
+        let expected_results: Vec<bool> =
+            addrs.iter()
+            .map(|addr: &Address| *addr == comm_line)
+            .collect();
+        let actual_results: Vec<bool> =
+            addrs.iter()
+            .map(|addr: &Address| comm.should_run(*addr))
+            .collect();
+        assert_eq!(actual_results, expected_results);
+    }
+
+    #[test]
+    fn empty_condition_always_runs() {
+        let addrs: Vec<Address> = (0..10).collect();
+        let comm = Command::new(0);
+        for curr_addr in addrs.iter() {
+            assert_eq!(comm.should_run(*curr_addr), true);
+        }
     }
 }
