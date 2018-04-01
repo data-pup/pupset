e8504dd6ca8a86d22d2972a3c66a9f2d926cb88e
data-pup <kevinmartinucr@gmail.com>
2018-04-01 15:54:13 -0700

    Added some more detail to the address condition parsing.

--- src/command/address_condition/parse.rs
+++ src/command/address_condition/parse.rs
@@ -4,20 +4,41 @@ use command::address_condition::{
 
 pub type AddressList = Vec<Address>;
 
+#[derive(Debug, PartialEq)]
+pub enum ParseError {
+    ArgEmpty,
+    InvalidLowerBounds,
+    InvalidUpperBounds,
+    InvalidAddressNumber,
+}
+
+pub type ParseResult = Result<AddressList, ParseError>;
+
 static LOWER_BOUNDS_CHARS: [char; 2] = ['[', '('];
+static UPPER_BOUNDS_CHARS: [char; 2] = [']', ')'];
 
-pub fn parse_arg(arg: String) -> Result<AddressList, String> {
-    arg.split(",");
-
-    unimplemented!();
+pub fn parse_arg(arg: String) -> ParseResult {
+    if arg.is_empty() { return Err(ParseError::ArgEmpty); }
+    Ok(vec![])
 }
 
 #[cfg(test)]
 mod parse_tests {
     use command::address_condition::parse::*;
+    // use command::address_condition::Address;
 
     #[test]
-    fn it_works() {
-        assert_eq!(1, 1);
+    fn empty_string_returns_err() {
+        let actual_result:   ParseResult = parse_arg(String::from(""));
+        let expected_result: ParseResult = Err(ParseError::ArgEmpty);
+        assert_eq!(actual_result, expected_result);
+    }
+
+    #[test]
+    fn parse_line_number() {
+        let arg = String::from("[10]");
+        let actual_result:   ParseResult = parse_arg(arg);
+        let expected_result: ParseResult = Ok(vec![10]);
+        assert_eq!(actual_result, expected_result);
     }
 }
