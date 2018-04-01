6a8c64823ba4eb28b276bf5c95e85981b1bbcfeb
data-pup <kevinmartinucr@gmail.com>
2018-03-31 22:23:04 -0700

    Added a very generic version of the command class. this still needs to be generalized so that the address condition is a trait object (Box<T> might be needed), and then implementing a run command.

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -1,1 +1,18 @@
 pub mod address_condition;
+use self::address_condition::AddressCondition;
+use self::address_condition::OneAddressCondition;
+use self::address_condition::line_number::LineNumber;
+
+pub struct Command {
+    cond: LineNumber,
+}
+
+#[cfg(test)]
+mod tests {
+    use command::*;
+
+    #[test]
+    fn it_works() {
+        Command { cond: LineNumber::new(0_32) };
+    }
+}
