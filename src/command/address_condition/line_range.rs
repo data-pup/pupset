8e5b56bbc72182dca9aa6501a324807f62371437
data-pup <kevinmartinucr@gmail.com>
2018-03-31 21:57:14 -0700

    Added some logic, range minimum is working. Functionality exists in these classes to allow for inclusive/exclusive bounds in ranges!

--- src/command/address_condition/line_range.rs
+++ src/command/address_condition/line_range.rs
@@ -4,19 +4,44 @@ use command::address_condition::{
     TwoAddressCondition,
 };
 
-// pub struct LineRange {
-//     min: Address,
-//     max: Address,
-//     min_is_inclusive: bool,
-//     max_is_inclusive: bool,
-// }
+pub struct LineRange {
+    min: RangeBounds,
+    max: RangeBounds
+}
+
+impl AddressCondition for LineRange {
+    fn applies(&self, current_line: Address) -> bool {
+        match self.min.is_inclusive {
+            true if  (self.min.val <= current_line) != true => return false,
+            false if (self.min.val < current_line)  != true => return false,
+            _ => { }
+        }
+        true
+    }
+}
+
+impl TwoAddressCondition for LineRange {
+    fn new(a: Address, b: Address) -> Self {
+        Self {
+            min: RangeBounds { val: a, is_inclusive: true  },
+            max: RangeBounds { val: b, is_inclusive: false },
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct RangeBounds { val: Address, is_inclusive: bool }
 
 #[cfg(test)]
 mod tests {
     use command::address_condition::line_range::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(1, 1);
+    fn min_works() {
+        let range = LineRange::new(5, 10);
+        assert_eq!(range.applies(4),  false);
+        assert_eq!(range.applies(5),  true);
+        assert_eq!(range.applies(9),  true);
+        // assert_eq!(range.applies(10), false);
     }
 }
