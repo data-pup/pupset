908ab3ac5ea9db1be9802e8358a24b7d08f877f7
data-pup <kevinmartinucr@gmail.com>
2018-04-01 17:15:44 -0700

    Added some logic for parsing the beginning of a string. This test now passes, similar logic can be used to identify the upper closure in an address condition.

--- src/command/address_condition/parse.rs
+++ src/command/address_condition/parse.rs
@@ -1,3 +1,5 @@
+use std::ops::Index;
+
 use command::address_condition::{
     Address
 };
@@ -17,10 +19,15 @@ static LOWER_BOUNDS_CHARS: [char; 2] = ['[', '('];
 static UPPER_BOUNDS_CHARS: [char; 2] = [']', ')'];
 static MINIMUM_LENGTH: usize = 3;
 
-pub fn parse_arg(arg: String) -> ParseResult {
+pub fn parse_arg(arg: &String) -> ParseResult {
     if arg.is_empty() { return Err(ParseError::ArgEmpty); }
-    if arg.len() < MINIMUM_LENGTH { return Err(ParseError::ArgTooShort); }
     Ok(vec![])
+}
+
+pub fn arg_starts_with_closure(arg: &String) -> bool {
+    ["[", "("].iter()
+        .map(|closure| arg.starts_with(closure))
+        .fold(false, |acc, elem| acc || elem)
 }
 
 #[cfg(test)]
@@ -29,29 +36,32 @@ mod parse_tests {
 
     #[test]
     fn empty_string_returns_err() {
-        let actual_result:   ParseResult = parse_arg(String::from(""));
+        let actual_result:   ParseResult = parse_arg(&String::from(""));
         let expected_result: ParseResult = Err(ParseError::ArgEmpty);
         assert_eq!(actual_result, expected_result);
     }
 
     #[test]
-    fn empty_closure_returns_err() {
-        let actual_result:   ParseResult = parse_arg(String::from("[]"));
-        let expected_result: ParseResult = Err(ParseError::ArgTooShort);
-        assert_eq!(actual_result, expected_result);
-    }
-
-    #[test]
-    fn missing_closure_returns_err() {
-        let actual_result:   ParseResult = parse_arg(String::from("1"));
-        let expected_result: ParseResult = Err(ParseError::ArgTooShort);
-        assert_eq!(actual_result, expected_result);
+    fn test_arg_starts_with_closure() {
+        let test_cases: Vec<(String, bool)> = vec![
+            (String::from("[1]"), true),
+            (String::from("(1)"), true),
+            (String::from("1"), false),
+            (String::from("]1["), false),
+            (String::from(")1("), false),
+        ];
+        let run_test = |test: &(String, bool)| {
+            let &(ref arg, ref expected_result) = test;
+            let actual_result = arg_starts_with_closure(arg);
+            assert_eq!(actual_result, *expected_result, "Test failed for input: '{}'", arg);
+        };
+        test_cases.iter().for_each(|test| run_test(test));
     }
 
     #[test]
     fn parse_line_number() {
         let arg = String::from("[10]");
-        let actual_result:   ParseResult = parse_arg(arg);
+        let actual_result:   ParseResult = parse_arg(&arg);
         let expected_result: ParseResult = Ok(vec![10]);
         assert_eq!(actual_result, expected_result);
     }
