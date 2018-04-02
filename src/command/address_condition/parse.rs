8cdc6bf1b27cbf4ad6d1b042bb4f504e814cfca1
data-pup <kevinmartinucr@gmail.com>
2018-04-01 19:38:19 -0700

    Dividing sections of the parsing code into logical submodules.

--- src/command/address_condition/parse.rs
+++ src/command/address_condition/parse.rs
@@ -1,5 +1,3 @@
-use std::ops::Index;
-
 use command::address_condition::{
     Address
 };
@@ -9,30 +7,42 @@ pub type AddressList = Vec<Address>;
 #[derive(Debug, PartialEq)]
 pub enum ParseError {
     ArgEmpty,
-    ArgTooShort,
+    MissingClosuresError,
     InvalidLowerBounds,
     InvalidUpperBounds,
     InvalidAddressNumber,
 }
 
-static LOWER_BOUNDS_CHARS: [char; 2] = ['[', '('];
-static UPPER_BOUNDS_CHARS: [char; 2] = [']', ')'];
-static MINIMUM_LENGTH: usize = 3;
-
 pub fn parse_arg(arg: &String) -> ParseResult {
     if arg.is_empty() { return Err(ParseError::ArgEmpty); }
+    if !cond_closures::arg_starts_with_closure(&arg)
+    || !cond_closures::arg_ends_with_closure(&arg) {
+        return Err(ParseError::MissingClosuresError);
+    }
     Ok(vec![])
 }
 
-pub fn arg_starts_with_closure(arg: &String) -> bool {
-    ["[", "("].iter()
-        .map(|closure| arg.starts_with(closure))
-        .fold(false, |acc, elem| acc || elem)
+mod cond_closures {
+    static LOWER_BOUNDS_CHARS: [&str; 2] = ["[", "("];
+    static UPPER_BOUNDS_CHARS: [&str; 2] = ["]", ")"];
+
+    pub fn arg_starts_with_closure(arg: &String) -> bool {
+        LOWER_BOUNDS_CHARS.iter()
+            .map(|closure| arg.starts_with(closure))
+            .fold(false, |acc, elem| acc || elem)
+    }
+
+    pub fn arg_ends_with_closure(arg: &String) -> bool {
+        UPPER_BOUNDS_CHARS.iter()
+            .map(|closure| arg.ends_with(closure))
+            .fold(false, |acc, elem| acc || elem)
+    }
 }
 
 #[cfg(test)]
 mod parse_tests {
     use command::address_condition::parse::*;
+    use command::address_condition::parse::cond_closures::*;
 
     #[test]
     fn empty_string_returns_err() {
@@ -53,6 +63,23 @@ mod parse_tests {
         let run_test = |test: &(String, bool)| {
             let &(ref arg, ref expected_result) = test;
             let actual_result = arg_starts_with_closure(arg);
+            assert_eq!(actual_result, *expected_result, "Test failed for input: '{}'", arg);
+        };
+        test_cases.iter().for_each(|test| run_test(test));
+    }
+
+    #[test]
+    fn test_arg_ends_with_closure() {
+        let test_cases: Vec<(String, bool)> = vec![
+            (String::from("[1]"), true),
+            (String::from("(1)"), true),
+            (String::from("1"), false),
+            (String::from("]1["), false),
+            (String::from(")1("), false),
+        ];
+        let run_test = |test: &(String, bool)| {
+            let &(ref arg, ref expected_result) = test;
+            let actual_result = arg_ends_with_closure(arg);
             assert_eq!(actual_result, *expected_result, "Test failed for input: '{}'", arg);
         };
         test_cases.iter().for_each(|test| run_test(test));
